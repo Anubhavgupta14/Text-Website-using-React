ae47c213b2caa9d29e948e813d22089459a8c79b
Anubhavgupta14 <anubhavgu2002@gmail.com>
2022-11-16 01:25:20 +0530

    Update

--- src/Components/Textform.js
+++ src/Components/Textform.js
@@ -7,6 +7,11 @@ export default function Textform(Prop) {
         let a = text.toUpperCase();
         setText(a);
     }
+    const handellowclick = ()=>{
+      //console.log("Clicked");
+      let a = text.toLowerCase();
+      setText(a);
+  }
     const handleonchange=(event)=>{
         //console.log("ok");
         setText(event.target.value);
@@ -19,7 +24,8 @@ export default function Textform(Prop) {
     <label htmlFor="exampleFormControlTextarea1" className="form-label">{Prop.heading}</label>
     <textarea className="form-control" id="exampleFormControlTextarea1" rows="8" value={text} onChange={handleonchange}></textarea>
     </div>
-    <button className='btn btn-primary' onClick={handelupclick}>UpperCase</button>
+    <button className='btn btn-primary mx-2' onClick={handelupclick}>UpperCase</button>
+    <button className='btn btn-primary' onClick={handellowclick}>LowerCase</button>
     <div className='container my-3'>
       <h1>Your Text Summary</h1>
       <p>{text.split(" ").length} words and {text.length} Character Counts</p>

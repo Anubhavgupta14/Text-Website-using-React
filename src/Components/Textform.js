1c323071a13315da0a75febdae2d5a43463c1c51
Anubhavgupta14 <anubhavgu2002@gmail.com>
2022-11-23 01:01:10 +0530

    UPdate

--- src/Components/Textform.js
+++ src/Components/Textform.js
@@ -30,12 +30,12 @@ const handelextraspace = ()=>{
     <>
     <div className="mb-3">
     <label htmlFor="exampleFormControlTextarea1" className="form-label">{Prop.heading}</label>
-    <textarea className="form-control" id="exampleFormControlTextarea1" style={{backgroundColor: Prop.mode==='dark'?'grey':'white', color: Prop.mode==='dark'?'white':'black'}} rows="8" value={text} onChange={handleonchange}></textarea>
+    <textarea className="form-control" id="exampleFormControlTextarea1" style={{backgroundColor: Prop.mode==='dark'?'rgb(51 74 250)':'white', color: Prop.mode==='dark'?'white':'black'}} rows="8" value={text} onChange={handleonchange}></textarea>
     </div>
-    <button className='btn btn-primary mx-2 my-2' onClick={handelupclick}>UpperCase</button>
-    <button className='btn btn-primary mx-2 my-2' onClick={handellowclick}>LowerCase</button>
-    <button className='btn btn-primary mx-2 my-2' onClick={handelcleclick}>Clear</button>
-    <button className='btn btn-primary mx-2 my-2' onClick={handelextraspace}>Remove Extra Space</button>
+    <button disabled={text.length===0} className='btn btn-primary mx-2 my-2' onClick={handelupclick}>UpperCase</button>
+    <button disabled={text.length===0} className='btn btn-primary mx-2 my-2' onClick={handellowclick}>LowerCase</button>
+    <button disabled={text.length===0} className='btn btn-primary mx-2 my-2' onClick={handelcleclick}>Clear</button>
+    <button disabled={text.length===0} className='btn btn-primary mx-2 my-2' onClick={handelextraspace}>Remove Extra Space</button>
     <div className='container my-3'>
       <h1>Your Text Summary</h1>
       <p>{text.split(" ").filter((element)=>{return element.length!==0}).length} words and {text.length} Character Counts</p>

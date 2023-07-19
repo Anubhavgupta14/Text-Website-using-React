612ec9ab699c313657c65273bdbabdda06d371ce
Anubhavgupta14 <anubhavgu2002@gmail.com>
2023-07-19 16:26:34 +0530

    fix

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import propTypes from 'prop-types'
 import { Link } from 'react-router-dom'
-import img from '../images/img.png'
+import logo from '../images/img.png'
 export default function Navbar(props) {
   return (
     <>
      <nav className={`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode}`}>
   <div className="container-fluid">
-    <img src={img} alt='image' className="mx-2"/>
+    <img src={logo} alt='something' className="mx-2"/>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
@@ -25,9 +25,9 @@ export default function Navbar(props) {
     Colored Background
   </button>
   <ul className="dropdown-menu">
-    <li><a className="dropdown-item" onClick={props.changegreen} href="#">Green</a></li>
-    <li><a className="dropdown-item" onClick={props.changered} href="#">Red</a></li>
-    <li><a className="dropdown-item" onClick={props.changeyellow} href="#">Yellow</a></li>
+    <li><a className="dropdown-item" onClick={props.changegreen} href="/#">Green</a></li>
+    <li><a className="dropdown-item" onClick={props.changered} href="/#">Red</a></li>
+    <li><a className="dropdown-item" onClick={props.changeyellow} href="/#">Yellow</a></li>
   </ul>
 </div>
       <div className={`form-check form-switch text-${props.mode==='light'?'dark':'light'} my-2`}>
